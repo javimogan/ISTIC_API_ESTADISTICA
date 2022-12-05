70e42ce06a242e68697847be51abd6218f4c6341
javimogan <diazalonsojavi@gmail.com>
2022-12-05 21:14:20 +0100

    fixing errors

--- service/GlobalService.js
+++ service/GlobalService.js
@@ -1,18 +1,20 @@
 'use strict';
-
 const max = 9999
 const min = 0
+
 /**
  * obtiene el número de caídas de un usuario en el último mes.
  * obtiene el número de caídas de un usuario en el último mes.
  *
  * id Integer id del usuario
- * returns Integer
+ * returns Value
  **/
 exports.getFalls = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = Math.floor(Math.random() * (max - min + 1) + min);
+    examples['application/json'] = {
+  "value" : Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -27,12 +29,14 @@ exports.getFalls = function(id) {
  * obtiene la velocidad media de un usuario en el último mes.
  *
  * id Integer id del usuario
- * returns Integer
+ * returns Value
  **/
 exports.obtinfoavgspeed = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = Math.floor(Math.random() * (max - min + 1) + min);
+    examples['application/json'] = {
+  "value" : Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -47,12 +51,14 @@ exports.obtinfoavgspeed = function(id) {
  * obtiene el número de kilómetros recorridos por un usuario en el último mes.
  *
  * id Integer id del dispositivo
- * returns Integer
+ * returns Value
  **/
 exports.obtinfokms = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = Math.floor(Math.random() * (max - min + 1) + min);
+    examples['application/json'] = {
+  "value" : Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -67,12 +73,14 @@ exports.obtinfokms = function(id) {
  * obtiene las ubicaciones frecuentes de un usuario.
  *
  * id Integer id del usuario
- * returns Integer
+ * returns Value
  **/
 exports.obtinfolocations = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = Math.floor(Math.random() * (max - min + 1) + min);
+    examples['application/json'] = {
+  "value": Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -87,12 +95,14 @@ exports.obtinfolocations = function(id) {
  * obtiene la velocidad máxima de un usuario en el último mes.
  *
  * id Integer id del usuario
- * returns Integer
+ * returns Value
  **/
 exports.obtinfomaxspeed = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = Math.floor(Math.random() * (max - min + 1) + min);
+    examples['application/json'] = {
+  "value": Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -107,12 +117,14 @@ exports.obtinfomaxspeed = function(id) {
  * obtiene la puntuación de un usuario. Métrica obtenida en función del número de kilómetros recorridos y los accidentes involucrados en un mes.
  *
  * id Integer id del usuario
- * returns Integer
+ * returns Value
  **/
 exports.obtinfoscore = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = Math.floor(Math.random() * (max - min + 1) + min);
+    examples['application/json'] = {
+  "value": Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -127,12 +139,14 @@ exports.obtinfoscore = function(id) {
  * número de llamadas a emergencias en el último mes causadas por la caída de un usuario.
  *
  * id Integer id del usuario
- * returns Integer
+ * returns Value
  **/
 exports.obtinfosos = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = 2;
+    examples['application/json'] = {
+  "value": Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -147,12 +161,14 @@ exports.obtinfosos = function(id) {
  * obtiene las horas de uso por un usuario en el último mes.
  *
  * id Integer id del usuario
- * returns Integer
+ * returns Value
  **/
 exports.obtinfotime = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = Math.floor(Math.random() * (max - min + 1) + min);
+    examples['application/json'] = {
+  "value": Math.floor(Math.random() * (max - min + 1) + min)
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
